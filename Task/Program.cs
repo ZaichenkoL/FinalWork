fe9a5eefecee807fe0ec6c32b5450bf106c3234f
LarisaZaichenko <zlarra@yandex.ru>
2022-10-17 22:39:36 +0300

    added task solution

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+
+string[] array1 = new string[] {"cat", "false", "-1", "1474", "red"};
+string[] array2 = new string[array1.Length];
+SecondArray(array1, array2);
+PrintArray(array2);
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length<=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
+
